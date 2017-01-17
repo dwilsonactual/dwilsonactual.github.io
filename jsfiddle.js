56d2ca499ea275916a11ff9350b872d7f3eda0f7
David Wilson (VSNC) <dawilson@microsoft.com>
2017-01-17 15:44:17 -0500

    Add jsfiddle.js

---
+++ jsfiddle.js
@@ -0,0 +1,14 @@
+function log(msg) {
+	var out = document.getElementById('out');
+    if (!out) {
+    	out = document.createElement('ul');
+        out.setAttribute('id', 'out');
+        document.body.appendChild(out);
+    }
+	var li = document.createElement('li');
+    li.textContent = msg;
+    out.appendChild(li);
+}
+function clearLog() {
+	document.getElementById('out').innerHTML = '';
+}
